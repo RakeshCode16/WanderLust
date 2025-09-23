a038b4f8c6d1bfdbcc56fb7055b2f7736b5742f1
RakeshCode16 <rakesh2225847105@gmail.com>
2025-09-23 20:36:03 +0530

    Updated existing files with latest changes

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate =require ("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js")
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const MONGO_URL ="mongodb://127.0.0.1:27017/wanderlust" ;
 
@@ -29,57 +36,51 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie : {
+        expires : Date.now() + 7 *24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly : true,
+    },
+};
+
 app.get("/", (req, res)=>{
     res.send("Hi, I am root");
 });
 
-// Index Route
-app.get("/listings", wrapAsync(async (req, res)=>{
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
-}));
+app.use(session(sessionOptions));
+app.use(flash());
 
-// New Route
-app.get("/listings/new", (req, res)=>{
-    res.render("listings/new.ejs");
+app.use (passport.initialize());
+app.use (passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req , res, next)=>{
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
+    next();
 });
 
-// Show Route
-app.get("/listings/:id", wrapAsync(async(req, res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
-}));
+// app.get("/demouser", async(req, res)=>{
+//     let fakeUser = new User({
+//         email: "student@gmail.com",
+//         username: "delta-student",
+//     });
 
-// Create Route
-app.post("/listings", wrapAsync(async (req, res, next) =>{
-        const newListing = new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
-     }));
+//     let registeredUser = await User.register(fakeUser, "helloworld");
+//     res.send(registeredUser);
+// });
 
-// Edit Route
-app.get("/listings/:id/edit", wrapAsync(async(req, res)=>{
-    let{id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
-}));
-
-// Update Route
-app.put("/listings/:id",wrapAsync(async (req, res)=>{
-    let{id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}));
-
-// Delete Route
-app.delete("/listings/:id", wrapAsync(async(req, res)=>{
-    let {id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
-
+app.use("/", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 app.all(/.*/, (req, res, next) => {
            next(new ExpressError(404, "Page Not Found"));
@@ -87,9 +88,9 @@ app.all(/.*/, (req, res, next) => {
 
 app.use((err, req, res, next)=>{
     let {statusCode=500, message = "Something went wrong!"} = err; 
-    res.status(statusCode).send(message);
+    res.status (statusCode).render("error.ejs", {message});
+    // res.status(statusCode).send(message);
 });
-
 
 app.listen(8080, ()=>{
     console.log("server is listening to port 8080");
